366fa67b4eeea9e2b9180903b652146a0c13e761
Luan Oliveira <engluanoliv@gmail.com>
2024-03-19 18:50:36 -0300

    Revert "problems to push the final file"
    This reverts commit 0454d4d6b6fd51a63864628595bd0e88a2e873c7.

--- src/components/HamburguerNavComponent/HamburguerNav.jsx
+++ src/components/HamburguerNavComponent/HamburguerNav.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { device } from "../../utils/responsiveSettings";
 
 const Icon = styled.div`
   display: none;
-  @media only screen and (max-width: 864px) {
+  @media only screen and (${(props) => device[props.$device]}) {
     display: block;
     cursor: pointer;
   }
@@ -14,17 +16,36 @@ const BarIcon = styled.div`
   height: 3px;
   margin: 5px auto;
   border-radius: 10px;
-  -webkit-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
   background-color: #fff;
+  &:nth-child(1) {
+    transform: ${({ open }) =>
+      open ? "rotate(45deg) translateY(11px)" : "rotate(0)"};
+  }
+  &:nth-child(2) {
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(0)")};
+    opacity: ${({ open }) => (open ? 0 : 1)};
+  }
+  &:nth-child(3) {
+    transform: ${({ open }) =>
+      open ? "rotate(-45deg) translateY(-11px)" : "rotate(0)"};
+  }
 `;
 
-export default function HamburguerNav({ ...props }) {
+export default function HamburguerNav({ onOpen, ...props }) {
+  const [open, setOpen] = useState(false);
+
+  function handleClick() {
+    const newOpen = !open;
+    setOpen(newOpen);
+    onOpen(newOpen);
+  }
+
   return (
-    <Icon {...props}>
-      <BarIcon></BarIcon>
-      <BarIcon></BarIcon>
-      <BarIcon></BarIcon>
+    <Icon $device="tablet" open={open} onClick={handleClick} {...props}>
+      <BarIcon open={open}></BarIcon>
+      <BarIcon open={open}></BarIcon>
+      <BarIcon open={open}></BarIcon>
     </Icon>
   );
 }
